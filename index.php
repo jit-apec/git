764d47b3b440afa2bad411043b0f282527bb2f28
ravat jitendra <ravatjit777@gmail.com>
2021-06-09 09:20:35 -0700

    initial status

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    define('Company','github');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width-device-width,initial-scale1.0">
+        <title>github </title>
+    </head>
+    <body>
+        <h1><?=Company ?></h1>
+    </body>
+</html>
